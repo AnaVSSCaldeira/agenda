2853c0aba967b2f5037d2138224ba62ddd6b465a
Ana Caldeira <anavsscaldeira@gmail.com>
2023-11-28 15:18:25 -0300

    image configuration

--- project/settings.py
+++ project/settings.py
@@ -89,4 +89,9 @@ STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
